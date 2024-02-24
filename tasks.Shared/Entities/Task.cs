b9cf362d1b7400a0c13fedf1eba18c87c70d50ab
Roman Dzhulai <rdzhulai@gmail.com>
2024-02-24 09:40:49 +0100

    Add the Task Entity.

---
+++ tasks.Shared/Entities/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tasks.Shared.Entities {
+    internal class Task {
+        [Key]
+        public int Id { get; set; }
+        [Column]
+        [Required]
+        public required string Text { get; set; }
+        [Column]
+        [DefaultValue(false)]
+        public bool Completed { get; set; }
+    }
+}
